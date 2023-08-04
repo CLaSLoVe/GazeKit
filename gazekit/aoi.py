5abb6147bc430419965830d07c514965ff928282
CLaSLoVe <jindian_0805@foxmail.com>
2023-08-04 13:51:47 +0800

    better doc

--- gazekit/aoi.py
+++ gazekit/aoi.py
@@ -5,22 +5,74 @@ import numpy as np
 
 
 class AOI:
-    def __init__(self, x, y, w, h, display, name, panel=None):
-        assert len(display) == 2
-        self.x = x/display[0]
-        self.y = y/display[1]
-        self.w = w/display[0]
-        self.h = h/display[1]
+    def __init__(self, x, y, w, h, name, display=(1, 1), panel=None):
+        """
+        Initializes an AOI (Area of Interest) object.
+
+        :param x: The x-coordinate of the top-left corner of the AOI in pixels.
+        :param y: The y-coordinate of the top-left corner of the AOI in pixels.
+        :param w: The width of the AOI in pixels.
+        :param h: The height of the AOI in pixels.
+        :param name: The name of the AOI.
+        :param display: The display dimensions in pixels, represented as (width, height).
+        :param panel: The specific functionality of the AOI, represented as a list.
+        :raises AssertionError: If the length of the `display` tuple is not 2.
+
+        """
+        assert len(display) == 2, "The display tuple must have exactly 2 elements."
+
+        self.x = x / display[0]
+        self.y = y / display[1]
+        self.w = w / display[0]
+        self.h = h / display[1]
         self.name = name
         self.panel = panel
 
-        self.c = np.array([self.x+self.w/2, self.y+self.h/2])
-        self.r = np.array([self.w/2, self.h/2])
+        self.c = np.array([self.x + self.w / 2, self.y + self.h / 2])
+        self.r = np.array([self.w / 2, self.h / 2])
+
+
+def xy2aoi(xy: np.ndarray, centers: list, radius: list) -> np.ndarray:
+    """
+    Converts a set of XY coordinates to AOIs (Areas of Interest) based on specified centers and radii.
+
+    :param xy: An array of XY coordinates.
+    :type xy: numpy.ndarray
+    :param centers: A list of center coordinates for each AOI.
+    :type centers: list
+    :param radius: A list of radii for each AOI.
+    :type radius: list
+    :return: An array representing the AOIs where each column corresponds to an AOI and each row corresponds to a coordinate in `xy`.
+    :rtype: numpy.ndarray
+    """
+    aois = []
+
+    for i in range(len(centers)):
+        center = centers[i]
+        radiu = radius[i]
+        in_set = np.abs(xy - center) < radiu
+        dist_xy = np.logical_and.reduce(in_set.T, axis=0)
+        aois.append(dist_xy)
+
+    return np.array(aois).T
 
 
 def read_aoi_ini_file(file_path: str, display) -> List[AOI]:
+    def read_aoi_ini_file(file_path: str, display) -> List[AOI]:
+        """
+        Read an AOI (.ini) file and return a list of AOI objects.
+
+        :param file_path: the path to the AOI (.ini) file.
+        :type file_path: str
+        :param display: the display object to use for converting coordinates from pixels to degrees of visual angle.
+        :type display: Display
+        :return: a list of AOI objects representing the areas of interest defined in the AOI file.
+        :rtype: list[AOI]
+        :raises FileNotFoundError: if the AOI file specified by `file_path` does not exist.
+        """
+
     if not os.path.isfile(file_path):
-        raise FileNotFoundError("No [aoi].ini file!")
+        raise FileNotFoundError(f"Could not find file: {file_path}")
     # read ini
     config = configparser.ConfigParser()
     config.read(file_path)
@@ -37,16 +89,6 @@ def read_aoi_ini_file(file_path: str, display) -> List[AOI]:
         aois.append(aoi)
     return aois
 
-
-def xy2aoi(xy, centers, radius):
-    aois = []
-    for i in range(len(centers)):
-        center = centers[i]
-        radiu = radius[i]
-        in_set = (np.abs(xy - center) < radiu)
-        dist_xy = np.logical_and.reduce(in_set.T, axis=0)
-        aois.append(dist_xy)
-    return np.array(aois).T
 
 if __name__ == '__main__':
     xy = np.array([[2,2],[2,3],[2,4]])
--- gazekit/sequence.py
+++ gazekit/sequence.py
@@ -1,13 +1,33 @@
 import numpy as np
 import pandas as pd
+import warnings
+
 from denoise import *
 from aoi import *
-import warnings
 from tools import *
 
 class Sequence:
     def __init__(self, x, y, t, d=None, aoi=None, history=None):
-        assert x[0], "Empty sequence."
+        """
+        Create a new `Sequence` object to represent eye-tracking data.
+
+        :param x: array of x-coordinates of eye positions.
+        :type x: array-like
+        :param y: array of y-coordinates of eye positions.
+        :type y: array-like
+        :param t: array of timestamps of eye positions, relative to the start of the recording.
+        :type t: array-like
+        :param d: array of durations of eye movements, or `None` if not available.
+        :type d: array-like or None
+        :param aoi: array of area-of-interest information for each eye position, or `None` if not available.
+                    If provided, this should be a list of `AOI` objects representing the areas of interest.
+        :type aoi: array-like or None
+        :param history: list of strings representing the processing history of the `Sequence` object, or `None` if not
+                        available.
+        :type history: list[str] or None
+        """
+        if len(x) == 0:
+            raise ValueError("Cannot create Sequence object with empty x array.")
         data = {'x': x,
                 'y': y,
                 't': np.array(t) - t[0]}
@@ -27,6 +47,13 @@ class Sequence:
         return self.data
 
     def loc_aoi(self, aoi_list):
+        """
+        Locate areas of interest (AOIs) in the eye-tracking data.
+
+        :param aoi_list: a list of `AOI` objects representing the areas of interest to locate.
+        :type aoi_list: list[AOI]
+        """
+        self.aoi_list = aoi_list
         if 'aoi' in self.data.columns:
             warnings.warn("exist AOI will be replaced!", UserWarning)
         centers = []
@@ -38,7 +65,9 @@ class Sequence:
         centers = np.array(centers)
         radius = np.array(radius)
         aoi_array = xy2aoi(xy, centers, radius)
-        self.data['aoi'] = encode_bool_lists(aoi_array)
+        self.data['aoi_encode'] = encode_bool_lists(aoi_array)
+        for i, aoi in enumerate(aoi_list):
+            self.data[aoi.name] = aoi_array[:, i]
 
     def detect_fixations(self, min_duration=0.1, max_dispersion=0.0004):
         """Detect fixations from eye-tracking data."""
@@ -52,13 +81,31 @@ class Sequence:
         ...
 
     def denoise(self, func, **kwargs):
-        """Denoise eye-tracking data"""
+        """
+        Denoise eye-tracking data using a specified function.
+
+        :param func: function to use for denoising. This function should take a `Sequence` object as its first argument,
+                     and any additional keyword arguments passed to `denoise` should be passed to `func`.
+        :type func: function
+        :param **kwargs: keyword arguments to pass to the denoising function `func`.
+        :type **kwargs: dict
+        :return: denoised `Sequence` object.
+        :rtype: Sequence
+        """
         pack = [self.data[col].values for col in self.data.columns]
         self.info.append('denoise with'+func.__name__)
         return func(self, **kwargs)
 
     def plot(self, func, **kwargs):
-        ...
+        """
+        Plot eye-tracking data.
+
+        :param func: function to use for plotting. This function should take a `Sequence` object as its first argument,
+                     and any additional keyword arguments passed to `plot` should be passed to `func`.
+        :type func: function
+        :param **kwargs: keyword arguments to pass to the plotting function `func`.
+        :type **kwargs: dict
+        """
 
 
 if __name__ == '__main__':
