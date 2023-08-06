fc66480a65c44cf97f6da6b3991482d15ba17fd9
CLaSLoVe <jindian_0805@foxmail.com>
2023-08-06 09:29:15 +0800

    bug fixed

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='gazekit',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.0.1',
     description='This is a Python library designed for processing eye-tracking data. It includes functions for noise reduction, area of interest (AOI) localization, data analysis, and plotting. With this library, you can easily preprocess and analyze eye-tracking data to gain insights into human visual cognitive processes. Whether you are conducting research in psychology, neuroscience, or human-computer interaction, this library can help you streamline your data processing workflow and obtain more accurate and reliable results.',
     author= 'CLaSLoVe',
     license='MIT',
